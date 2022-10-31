259c4c20c1bb0ab63cc41b135be6ebadff6ee00e
sathyagimenes <sathya.gimenes@gmail.com>
2022-10-31 12:28:30 -0300

    first commit

---
+++ includes/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 12:23:11 by sde-cama          #+#    #+#             */
+/*   Updated: 2022/10/31 12:26:38 by sde-cama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <libft.h>
+
+# define FAIL 0
+# define SUCCESS 1
+
+typedef enum e_error
+{
+	INVALID_ARGUMENT,
+}	t_error;
+
+#endif
